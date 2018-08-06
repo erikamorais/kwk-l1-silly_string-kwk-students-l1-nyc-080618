35f3716595fa32cc802de98cf33ce71688191c29
erikamorais <erikamorais2001@gmail.com>
2018-08-06 18:56:59 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -29,9 +29,9 @@ puts "There are #{letter_count} in #{big_word.length}"
 
 # Challenge 5: Add "wow" and "mom" to big_word
 
-bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
+bigger_word = big_word + "wow" + "mom" # Use String concatenation to add "wow" and "mom" to big_word
 
-new_letter_count = bigger_word.size {wow}
+new_letter_count = bigger_word.size
 
 puts "There are now #{new_letter_count} in #{bigger_word}"
 
